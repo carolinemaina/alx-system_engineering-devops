ad8c5b84ed96713c2b9b7f254da8520be197b520
carolinemaina <57516721+carolinemaina@users.noreply.github.com>
2024-02-23 17:08:29 +0300

    using puppet to install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,7 @@
 #Installing a package using puppet
 
 exec {'pip_install_flask':
-  command => '/usr/bin/pip3 install Flask==2.1.0',
+  command => '/usr/bin/pip3 install flask==2.1.0',
   path    => '/usr/bin',
-  unless  => '/usr/bin/pip3 show Flask',
+  unless  => '/usr/bin/pip3 show flask',
 }
