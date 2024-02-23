31f10ae9ddddd80b33cb96f976a645736bca6d8a
carolinemaina <57516721+carolinemaina@users.noreply.github.com>
2024-02-23 16:56:29 +0300

    correct syntax error

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
 #Installing a package using puppet
 
-exec ('pip_install_flask':
+exec {'pip_install_flask':
   command => '/usr/bin/pip3 install Flask==2.1.0',
   path    => '/usr/bin',
   unless  => '/usr/bin/pip3 show Flask',
