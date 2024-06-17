38fb5cea3c15a331bc6bf97b8e673da6ce7904ef
carolinemaina <57516721+carolinemaina@users.noreply.github.com>
2024-06-17 17:02:05 +0300

    errors

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -9,7 +9,7 @@ def top_ten(subreddit):
         return None
     url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
     headers = {
-            'User-Agent': '"MyRedditSubCounter/1.0 (by /u/obams)"'}
+            'User-Agent': '"MyRedditSubCounter/1.0 (by /u/rije1)"'}
     try:
         response = requests.get(url, headers=headers, allow_redirects=False)
         if response.status_code == 200:
