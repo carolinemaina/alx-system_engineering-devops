4810cc368b25b5a16eac1630f2b499cac7327854
carolinemaina <57516721+carolinemaina@users.noreply.github.com>
2024-06-17 16:42:54 +0300

    Task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,23 +1,21 @@
 #!/usr/bin/python3
-"""Module for task 0"""
-
+"""subscriber count"""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Queries the Reddit API and returns the number of subscribers to the subreddit"""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "My-User-Agent"}
-
+    if subreddit is None:
+        return 0
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {
+            'User-Agent': '"MyRedditSubCounter/1.0 (by /u/obams)"'}
     try:
         response = requests.get(url, headers=headers, allow_redirects=False)
-        if response.status_code >= 300:
-            return 0
-
-        data = response.json().get("data")
-        if data:
-            return data.get("subscribers", 0)
+        if response.status_code == 200:
+            data = response.json()
+            subscribers = data['data']['subscribers']
+            return subscribers
         else:
             return 0
-    except requests.RequestException:
+    except Exception as e:
         return 0
