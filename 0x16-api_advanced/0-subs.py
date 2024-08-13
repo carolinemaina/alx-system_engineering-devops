540b92f89c3f7945b61d0a44c9e3f5d761a015a2
carolinemaina <57516721+carolinemaina@users.noreply.github.com>
2024-08-13 19:57:47 +0300

    errors

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,17 +1,17 @@
-import requests
-
+import urllib.request
+import json
 
 def number_of_subscribers(subreddit):
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {"User-Agent": "python: subreddit.subscriber.count: v1.0
-               (by / u / yourusername)"}
-
+    headers = {"User-Agent": "python:subreddit.subscriber.count:v1.0 (by /u/yourusername)"}
+    
     try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        if response.status_code == 200:
-            data = response.json()
-            return data['data']['subscribers']
-        else:
-            return 0
-    except requests.RequestException:
-        return 0
+        req = urllib.request.Request(url, headers=headers)
+        with urllib.request.urlopen(req) as response:
+            if response.status == 200:
+                data = json.loads(response.read().decode())
+                return data['data']['subscribers']
+            else:
+                return 0
+            except:
+                return 0                                                                                                                            
