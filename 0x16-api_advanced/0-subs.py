834f753380ca9c109dfc043f96a7cf112e3449f6
carolinemaina <57516721+carolinemaina@users.noreply.github.com>
2024-08-14 16:54:54 +0300

    errors

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,8 +4,10 @@ import requests
 
 def number_of_subscribers(subreddit):
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {"User-Agent": "python: subreddit.subscriber.count: v1.0(by /u/yourusername)"}
-
+    headers = {
+            "User-Agent": "python: subreddit.subscriber.count: v1.0 "
+            "(by /u/yourusername)"
+            }
     try:
         response = requests.get(url, headers=headers, allow_redirects=False)
         if response.status_code == 200:
