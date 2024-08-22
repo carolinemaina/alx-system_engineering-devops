582c659b043d5130e0e4d441ea470c559e99565b
carolinemaina <57516721+carolinemaina@users.noreply.github.com>
2024-08-22 10:52:39 +0000

    task 1

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,10 +1,11 @@
 # fix user limit
+
 exec { 'increase-hard-file-limit-holberton-user':
-  command => 'sed -i "/holberton hard/s/4/50000/" /etc/security/limits.conf',
+  command => "sed -i '/^holberton hard/s/5/50000/' /etc/security/limits.conf",
   path    => '/usr/local/bin/:/bin/'
 }
 
-exec { 'increase-soft-file-limit-for-holberton-user':
-  command => 'sed -i "/holberton soft/s/5/50000/" /etc/security/limits.conf',
+exec { 'increase-soft-file-limit-holberton-user':
+  command => 'sed -i "/^holberton soft/s/4/50000/" /etc/security/limits.conf',
   path    => '/usr/local/bin/:/bin/'
 }
